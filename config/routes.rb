7889bda23c7a98feba6322ea090e960e366af918
allezz <aerbay@gmail.com>
2020-06-26 12:48:20 +0200

    Add route for comments index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   namespace :api do
-    resources :articles, only: [:index, :show]
+    resources :articles, only: %i[index show]
+    resources :comments, only: [:index]
   end
 end
