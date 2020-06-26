7b9ee780de5c31faddfb96de58a6e69147998751
allezz <aerbay@gmail.com>
2020-06-26 12:37:59 +0200

    Specs for models are done

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,18 +1,21 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  describe "Articles has db columns" do
+  describe 'Articles has db columns' do
     it { is_expected.to have_db_column :title }
     it { is_expected.to have_db_column :body }
+    it { is_expected.to have_many :comments }
   end
 
-  describe "Validations" do
+  describe 'Validations' do
     it { is_expected.to validate_presence_of :title }
     it { is_expected.to validate_presence_of :body }
   end
 
-  describe "Factory" do
-    it "Should have valid factory" do
+  describe 'Factory' do
+    it 'Should have valid factory' do
       expect(create(:article)).to be_valid
     end
   end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,24 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Comment has db colums' do
+    it { is_expected.to have_db_column :body }
+  end
+
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of :body }
+  end
+
+  describe 'Relations' do
+    it { is_expected.to belong_to :user }
+    it { is_expected.to belong_to :article }
+  end
+
+  describe 'Factory' do
+    it 'Should have valid factory' do
+      expect(create(:comment)).to be_valid
+    end
+  end
 end
