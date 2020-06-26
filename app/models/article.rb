4fb110e8123a6b77e3b2fd76f566c9f0b845ea48
allezz <aerbay@gmail.com>
2020-06-26 12:28:41 +0200

    Add associations in models

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
 class Article < ApplicationRecord
   validates_presence_of :title, :body
+  has_many :comments
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
 class Comment < ApplicationRecord
+  validates_presence_of :body
   belongs_to :article
   belongs_to :user
 end
